ce7fad2221eab72bcce4a52339e09b17a5a5cdf8
Michael <github@harpelife.com>
2014-02-10 18:29:41 -0500

    Finished basic webserver controls.

--- webserver/engrave.php
+++ webserver/engrave.php
@@ -3,9 +3,23 @@
 
 function engrave($file, $title, $speed)
 {
-	$command = escapeshellcmd('sudo -i nohup /usr/custom/engravR/laserengraver -f ' . $file . ' -s ' . $speed . ' -t ' . $title . ' > /dev/null &');
+	$command = escapeshellcmd('sudo -i nohup /usr/custom/engravR/laserengraver -f ' . $file . ' -s ' . $speed . ' -t "' . $title . '" > /dev/null &');
 	$output = shell_exec($command);
 	
+}
+
+function validate()
+{
+	if (!isset($_POST["title"]) and !isset($_POST["speed"]))
+		return "Title and Speed are required.";
+	if ($_FILES["engraveFile"]["error"] > 0)
+		return "There was an error uploading the engraving file.";
+	if (substr($_FILES["engraveFile"]["name"], -4) != '.ngc')
+		return "The engraving file must be a ngc file format.";
+	if ($_POST['title'] == "")
+		return "Title must not be empty.";
+	if (!is_numeric($_POST["speed"]))
+		return "Speed must be a number";
 }
 
 
--- webserver/index.php
+++ webserver/index.php
@@ -1,12 +1,18 @@
 <?php
 
+	$error = "";
+	
 	if (isset($_POST["engrave"]))
 	{
-		if (isset($_POST["title"]) and isset($_POST["engraveFile"]) and isset($_POST["speed"]))
+		include 'engrave.php';
+		
+		$error = validate();
+		if ($error == "")
 		{
-			include 'engrave.php';
-			engrave($_POST["engraveFile"], $_POST["title"], $_POST["speed"]);
+			move_uploaded_file($_FILES["engraveFile"]["tmp_name"], "engrave.ngc");
+			engrave("/var/www/engrave.ngc", $_POST["title"], $_POST["speed"]);
 		}
+
 	}
 
 ?>
@@ -20,6 +26,7 @@
     <meta name="author" content="" />
 
     <title>EngravR Control</title>
+	<script data-require="jquery@*" data-semver="2.0.3" src="https://code.jquery.com/jquery-2.0.3.min.js"></script>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.0.1-p7/css/bootstrap.min.css">
 
@@ -37,8 +44,12 @@
 				<div class="container">
 					<h1>EngravR Control</h1>
 					<div class="well">
-						<form class="form-horizontal" action="" method="POST" id="engraving">
-
+						<form class="form-horizontal" action="" method="POST" id="engraving" enctype="multipart/form-data">
+							<div class="payment-errors"><?php if ($error != "") { ?><div class=" alert alert-danger"> 
+								<strong>Error!</strong>
+								<?php echo $error; ?></div><?php } ?>
+							</div>
+						
 							<div class="form-group">
 								<label for="title" class="col-lg-3 control-label">Engraving Title</label>
 								<div class="col-lg-9">
@@ -55,15 +66,19 @@
 							
 							
 							<div class="fileinput fileinput-new form-group" data-provides="fileinput">
-								<div class="input-group col-lg-9 col-offset-3">
+								<label for="engraveFile" class="col-lg-3 control-label">Engraving File</label>
+								<div class="input-group col-lg-9">
 									<div class="form-control uneditable-input span3" data-trigger="fileinput"><i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span></div>
 									<span class="input-group-addon btn btn-default btn-file"><span class="fileinput-new">Select Engraving File</span><span class="fileinput-exists">Change</span><input type="file" name="engraveFile" required></span>
 									<a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
 								</div>
 							</div>
 
+							<br><br>
 							<div class="form-group">
 								<input type='submit' class="btn btn-default btn-primary" name='engrave' value='Start Engraving'>
+								<div class=" alert alert-warning"><strong>Warning!</strong> Before submitting, make sure laser power is on and the laser safety is off.</div>
+							</div>
 							</div>
 						</form>
 					</div>
